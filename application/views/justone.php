9fec0ead67a1ddbffd8f4f3024371e6f373e2cde
A00770012 <A00770012bcit@gmail.com>
2015-03-29 14:48:09 -0700

    add the details of orders in justone Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/views/justone.php
+++ application/views/justone.php
@@ -1,3 +1,14 @@
-<div class="row">
-A burger?
+<div class="container">
+<h3>{order} for {customer} ({eatin}) </h3><br>
+
+{burgers}
+    <h4>*{header}*</h4>
+    Burger: {base} </br>
+    Cheese's: {cheeses} </br>
+    Sauce's: {sauces} </br>
+    Topping's: {toppings} </br>
+    <h5>Subtotal: ${subtotal} </h5> <br>
+{/burgers}
+<h4>Total: ${total} </h4>
+
 </div>
