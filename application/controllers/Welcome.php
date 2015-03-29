ee2bf3ae118880a6ac1542ba3d085bafe2e2b319
A00770012 <A00770012bcit@gmail.com>
2015-03-29 14:51:32 -0700

    fix error
    Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,17 +21,27 @@ class Welcome extends Application {
         // Build a list of orders
         // load the helper library of "directory"
         $this->load->helper('directory');
-        
 
         // store the directory path as an array
         $dir_map = directory_map(DATAPATH);
-
+        
+        $orders = array();
+        
         // seprate the xml file("order") and store to orders array
         foreach ($dir_map as $filename) {
             $file_info = pathinfo($filename);
-            if ($file_info['extension'] === strtolower('xml') && preg_match("/^[a-zA-Z]+\d+$/", $info['filename'])) {
+            if ($file_info['extension'] === strtolower('xml') && preg_match("/^[a-zA-Z]+\d+$/", $file_info['filename'])) {
                 $orders[] = array('order' => $filename);
             }
+        }
+        
+        // load the model of "Order"
+        $this->load->model('Order');
+        
+        foreach ($orders as &$arr) {
+            @$order = $this->Order->makeOrder($arr['order']);
+            $customer = $order['customer'];
+            $arr['customer'] = $customer;
         }
 
         // Present the list to choose from
@@ -46,8 +56,18 @@ class Welcome extends Application {
 
     function order($filename) {
         // Build a receipt for the chosen order
+        // load the model of "Order"
+        $this->load->model('Order');
+        $order = $this->Order->makeOrder($filename);
+
         // Present the list to choose from
         $this->data['pagebody'] = 'justone';
+        $this->data['customer'] = $order['customer'];
+        $this->data['order'] = $filename;
+        $this->data['burgers'] = $order['burgers'];
+        $this->data['eatin'] = $order['eatin'];
+        $this->data['total'] = $order['total'];
+
         $this->render();
     }
 
