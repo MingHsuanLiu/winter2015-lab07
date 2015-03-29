e5d06164d88c59e873b03cc27449f32eee891031
A00770012 <A00770012bcit@gmail.com>
2015-03-29 14:25:22 -0700

    load the directory helper and read the xml files and store the as arrray Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,36 +9,46 @@
  */
 class Welcome extends Application {
 
-    function __construct()
-    {
-	parent::__construct();
+    function __construct() {
+        parent::__construct();
     }
 
     //-------------------------------------------------------------
     //  Homepage: show a list of the orders on file
     //-------------------------------------------------------------
 
-    function index()
-    {
-	// Build a list of orders
-	
-	// Present the list to choose from
-	$this->data['pagebody'] = 'homepage';
-	$this->render();
+    function index() {
+        // Build a list of orders
+        // load the helper library of "directory"
+        $this->load->helper('directory');
+        
+
+        // store the directory path as an array
+        $dir_map = directory_map(DATAPATH);
+
+        // seprate the xml file("order") and store to orders array
+        foreach ($dir_map as $filename) {
+            $file_info = pathinfo($filename);
+            if ($file_info['extension'] === strtolower('xml') && preg_match("/^[a-zA-Z]+\d+$/", $info['filename'])) {
+                $orders[] = array('order' => $filename);
+            }
+        }
+
+        // Present the list to choose from
+        $this->data['pagebody'] = 'homepage';
+        $this->data['orders'] = $orders;
+        $this->render();
     }
-    
+
     //-------------------------------------------------------------
     //  Show the "receipt" for a specific order
     //-------------------------------------------------------------
 
-    function order($filename)
-    {
-	// Build a receipt for the chosen order
-	
-	// Present the list to choose from
-	$this->data['pagebody'] = 'justone';
-	$this->render();
+    function order($filename) {
+        // Build a receipt for the chosen order
+        // Present the list to choose from
+        $this->data['pagebody'] = 'justone';
+        $this->render();
     }
-    
 
 }
